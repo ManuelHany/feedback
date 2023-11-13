c3dd13bb49e4843bba4e93611681c5763c7b13a3
Manuel <manuelhany@gmail.com>
2023-11-13 14:48:08 +0200

    forms validation manual method

--- reviews/views.py
+++ reviews/views.py
@@ -7,6 +7,11 @@ from django.shortcuts import render
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']    # POST will hold the dictionary of the returned form data
+
+        if entered_username == "":
+            return render(request, "reviews/review.html", {
+                "has_error": True
+            })
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
     
