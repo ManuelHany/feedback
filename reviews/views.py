56e5d83cc06b9c0e679dde2055f19d13dd5827a1
Manuel <manuelhany@gmail.com>
2023-11-16 14:51:58 +0200

    Form View from django templates

--- reviews/views.py
+++ reviews/views.py
@@ -5,30 +5,26 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        
-        return render(request, "reviews/review.html", {
-        "form": form
-    })
+class ReviewView(FormView):
+    # put in consideratin that this FormView does not know what to do with the specific data. 
+    # you could want it to be saved to a file or anything else. 
+    # that is why we use the form_valid method.
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        
-        return render(request, "reviews/review.html", {
-        "form": form
-    })
 
 
 class ThankYouView(TemplateView):
