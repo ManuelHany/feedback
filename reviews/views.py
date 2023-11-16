5706c651065cd03046d319e7487df1389337fc4e
Manuel <manuelhany@gmail.com>
2023-11-16 12:17:46 +0200

    class views 1

--- reviews/views.py
+++ reviews/views.py
@@ -1,31 +1,39 @@
+from typing import Any
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import ReviewForm
-# from .models import Review
+from django.views import View
+from django.views.generic.base import TemplateView
 
-# existing_model = Review.objects.get(pk=1)
+from .forms import ReviewForm
+
+
 # Create your views here.
 
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        
+        return render(request, "reviews/review.html", {
+        "form": form
+    })
 
-def review(request):
-    if request.method == 'POST':
-        # existing_data = Review.objects.get(pk=1)
-        # form = ReviewForm(request.POST, instance=existing_data)
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/thank-you")
         
-    else:
-        form = ReviewForm()
-    
-    return render(request, "reviews/review.html", {
+        return render(request, "reviews/review.html", {
         "form": form
     })
 
 
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "this works!"
+        return context
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
