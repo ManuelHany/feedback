64cb1b8a2bd124212883ad7f1799c65d02eb3172
Manuel <manuelhany@gmail.com>
2023-11-21 17:02:02 +0200

    utilizing sessions

--- feedback/settings.py
+++ feedback/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR / "uploads"  #tells django where our files should be stored in general.
+MEDIA_URL = "/user-media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.ReviewView.as_view()),
     path("thank-you", views.ThankYouView.as_view()),
     path("reviews", views.ReviewsListView.as_view()),
+    path("reviews/favorite", views.AddFavoriteView.as_view()),   # we put the reviews/favorite first because we do not wand the favorite to be treaded as a primary key.
     path("reviews/<int:pk>", views.SingleReviewView.as_view())
 ]
