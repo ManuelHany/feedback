a30ba86b222957c934cee4982662132cc5cdfc55
Manuel <manuelhany@gmail.com>
2023-11-21 14:10:38 +0200

    using image field

--- feedback/settings.py
+++ feedback/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / "uploads"  #tells django where our files should be stored in general.
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
